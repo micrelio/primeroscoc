3672240d2ba32d328cf48af3a6308b053073bd0e
unknown <microa4@gmail.com>
2020-10-14 18:02:27 +0200

    reparado fallo ajuste de horas2 en dia de preparacion en dia de preparacion

--- web/php/guerras.php
+++ web/php/guerras.php
@@ -117,9 +117,9 @@
 										$di = substr($fechai, 6, 2); //sacamos numeros para dia
 										$mi = substr($fechai, 4, 2); //sacamos numeros para mes
 										$yi = substr($fechai, 0, 4); //sacamos numeros para año
-										$hie = substr($fechai, 9, 2) + 1; //sacamos 2 numero para horas
-										$hiu = substr($fechai, 9, 2) - 5; //sacamos 2 numero para horas
-										$him = substr($fechai, 9, 2) - 6; //sacamos 2 numero para horas
+										$hie = substr($fechai, 9, 2); //no se porque va mal ajuste de hora + 1; //sacamos 2 numero para horas
+										$hiu = substr($fechai, 9, 2); //no se porque va mal ajuste de hora - 5; //sacamos 2 numero para horas
+										$him = substr($fechai, 9, 2); //no se porque va mal ajuste de hora - 6; //sacamos 2 numero para horas
 										$ii = substr($fechai, 11, 2); //sacamos 2 2 numero para minutos
 										$si = substr($fechai, 13, 2); //sacamos numero para segundos
 										$inicio = $hie . "" . $ii/*."".$si*/;
@@ -173,7 +173,7 @@
 										$mex = "mex";
 										echo '<img src="../images/iconos/' . $esp . '.png" ; />';
 										$horaef = DateTime::createFromFormat('Hi', $final);
-										echo $horaef->format('H+1:i ');
+										echo $horaef->format('H:i ');
 										echo '<img src="../images/iconos/' . $usa . '.png" ; />';
 										$horauf = DateTime::createFromFormat('Hi', $finalu);
 										echo $horauf->format('H:i ');
